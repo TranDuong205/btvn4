9a74d7265bcd80e15384d8cbffe50f1458a7743b
TranDuong205 <duong.tranmanhduong.2005@gmail.com>
2024-08-20 12:33:34 +0700

    btvn

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import B8 from './admin/dashboard/page'
+import B2 from './bt1/B1'
+import B3 from '@component/button'
+export default function page() {
+  return (
+    <div>page
+      <B8></B8>
+      <B2></B2>
+      <B3></B3>
+    </div>
+  )
+}
